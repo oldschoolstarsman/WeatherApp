774e8b773ef44247c1457c75a154a54cb237e17c
oldschoolstarsman <gael_guyon@hotmail.com>
2020-01-28 14:31:44 +0100

    icon and images display accordingly

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,65 @@
+const container = document.querySelector('.container')
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
-const icon = document.querySelector('icon img');
+const icon = document.querySelector('.icon img');
+
 
 const updateUI = (data) => {
+    // remove display: none to display card when data is available
+    card.classList.contains('d-none') ? card.classList.remove('d-none') : true;
 
-    const {cityDetails, weatherInfo } = data;
+    if (data) {
+        const { cityDetails, weatherInfo } = data;
 
-    details.innerHTML = `
-        <h5 class="my-3">${cityDetails.EnglishName}</h5>
-        <div class="my-3">${weatherInfo.WeatherText}</div>
-        <div class="display-4 my-4">
-            <span>${weatherInfo.Temperature.Metric.Value}</span>
-            <span>&deg;C</span>
-        </div>
-        `;
+        // update cards detauls with data fetched
+        details.innerHTML = `
+            <h5 class="my-3">${cityDetails.EnglishName}</h5>
+            <div class="my-3">${weatherInfo.WeatherText}</div>
+            <div class="display-4 my-4">
+                <span>${weatherInfo.Temperature.Metric.Value}</span>
+                <span>&deg;C</span>
+            </div>
+            `;
 
-    card.classList.contains('d-none') ? card.classList.remove('d-none') : 1;
+        updateWeatherImage(weatherInfo);
+
+        updateWeatherIcon(weatherInfo);
+    }
 
 }
 
-const updateCity = async (city) => {
+//display day or night image from entered location
+const updateWeatherImage = (weatherInfo) => {
 
+    let timeSrc = null;
+
+    weatherInfo.IsDayTime ? timeSrc = 'img/day.svg' : timeSrc = 'img/night.svg';
+
+    time.setAttribute('src', timeSrc);
+
+}
+
+//display weather icon image from entered location current weather
+const updateWeatherIcon = (weatherInfo) => {
+
+    const iconSrc = `img/icons/${weatherInfo.WeatherIcon}.svg`;
+
+    icon.setAttribute('src', iconSrc);
+
+}
+
+
+const updateCity = async (city) => {
     const cityDetails = await getCity(city);
     const weatherInfo = await getWeather(cityDetails.Key)
 
-    return {cityDetails, weatherInfo};
-   
+    return { cityDetails, weatherInfo };
+
 }
 
+// handle form, get input, invoke functions to display info on page
 cityForm.addEventListener('submit', e => {
     e.preventDefault();
     const city = cityForm.city.value.trim();
@@ -37,5 +67,12 @@ cityForm.addEventListener('submit', e => {
 
     updateCity(city)
         .then(data => updateUI(data))
-        .catch(err => console.log(err));
+        .catch(err => { console.log(err)
+            // const errorMessage = 'Ooops';
+            // const el = document.createElement('div')
+            // container.appendChild(el).innerHTML = errorMessage;
+            // container.classList.add('error')
+        })
 })
+
+
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,11 +1,11 @@
-const key = '	1Nj6UWhFQw9HWcc0IB4TeASYoPySCQEZ';
+const key = 'KZ5KBAkqLhZJkLiPm4G87ULeiNryPiZk';
 
 //get weather info
 const getWeather = async (id) => {
-    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
+    const baseUrl = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`
 
-    const response = await fetch(base + query);
+    const response = await fetch(baseUrl + query);
     const data = await response.json();
 
     return data[0];
@@ -13,14 +13,17 @@ const getWeather = async (id) => {
 }
 
 
-
 const getCity = async (city) => {
-    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+
+    const baseUrl = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+
     const query = `?apikey=${key}&q=${city}`;
 
-    const response = await fetch(base + query);
+    const response = await fetch(baseUrl + query);
     const data = await response.json();
     return (data[0])
 };
+
+
 
 
